17fe28060cd9df36032f5616c651c06c1bb6e694
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2020-10-13 11:43:33 +0200

    chore: Disable integration tests
    The used API keys are not valid anymore. So for now
    we deactivate the integration tests

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@
  */
 export const GOOGLE_MAP_CLIENT_ID: string = undefined;
 export const GOOGLE_MAP_CHANNEL: string = undefined;
-export const GOOGLE_MAP_API = 'AIzaSyClZGdULfHFpEFI5XhwVBRk3olbh88oagQ';
-export const HERE_APP_CODE = 'g_EX8M9mlLEqrGlx_07MrA';
-export const HERE_APP_ID = 'DXLuEwxtBQ52jBUQCpQ9';
+export const GOOGLE_MAP_API = '';
+export const HERE_APP_CODE = '';
+export const HERE_APP_ID = '';
 // Degrees latitude per meter, used for (rough!) approximation of
 // viewbounds where the visible area is given in meters
 export const DEGRESS_PER_METER_LAT = 0.000008998719243599958;
